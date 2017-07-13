0a78c86eaf8c41a612302bf07b196a3e10825e4a
danneel_zhu <zhudanneel@gmail.com>
2017-07-13 20:55:10 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: danneel
+	> Mail: 819253203@qq.com
+	> Created Time: Thu Jul 13 20:54:21 2017
+ ************************************************************************/
+
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+    printf("hello world!\n");
+}
